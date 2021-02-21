22b08e39b3311c1d2667cd1ebd6a15033a62869c
root <qkskskzlr112@naver.com>
2021-02-21 12:54:17 +0000

    결제 시스템 라디오 버튼 기능

--- resources/views/iamport.blade.php
+++ resources/views/iamport.blade.php
@@ -21,7 +21,7 @@
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&family=Nanum+Pen+Script&display=swap');
 </style>
-<div class="container" style="font-family: 'Nanum Gothic', sans-serif; ">
+<div id="app" class="container" style="font-family: 'Nanum Gothic', sans-serif; ">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -36,19 +36,19 @@
                             <th scope="col">결제수단</th>
                             <td>
                                 <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                    <input class="form-check-input" type="radio" name="pay_method" id="inlineRadio1" value="card">
                                     <label class="form-check-label" for="inlineRadio1">신용카드</label>
                                 </div>
                                 <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                                    <input class="form-check-input" type="radio" name="pay_method" id="inlineRadio2" value="trans">
                                     <label class="form-check-label" for="inlineRadio2">실시간계좌이체</label>
                                 </div>
                                 <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                                    <input class="form-check-input" type="radio" name="pay_method" id="inlineRadio2" value="vbank">
                                     <label class="form-check-label" for="inlineRadio3">가상계좌</label>
                                 </div>
                                 <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                                    <input class="form-check-input" type="radio" name="pay_method" id="inlineRadio2" value="phone">
                                     <label class="form-check-label" for="inlineRadio4">휴대폰 결제</label>
                                 </div>
                             </td>
@@ -74,32 +74,37 @@
 <script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.1.5.js"></script>
 <script>
     function iamport_event() {
-        var IMP = window.IMP; // 생략가능
-        IMP.init('imp40035237'); // 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
-        IMP.request_pay({
-            pg: 'inicis', // version 1.1.0부터 지원.
-            pay_method: 'card',
-            merchant_uid: 'merchant_' + new Date().getTime(),
-            name: '주문명:진료비결제',
-            amount: @json($storage) ,
-            buyer_name: @json($patient->patient_name),
-            buyer_tel: @json($patient->phone_number),
-            buyer_addr: @json($patient->address),
-            buyer_postcode: @json($patient->postal_code),
-            m_redirect_url: 'http://34.234.79.156/index.php/patient/iamport_end/' + @json($patient->patient_id);
-        }, function(rsp) {
-            if (rsp.success) {
-                var msg = '결제가 완료되었습니다.';
-                msg += '고유ID : ' + rsp.imp_uid;
-                msg += '상점 거래ID : ' + rsp.merchant_uid;
-                msg += '결제 금액 : ' + rsp.paid_amount;
-                msg += '카드 승인번호 : ' + rsp.apply_num;
-            } else {
-                var msg = '결제에 실패하였습니다.';
-                msg += '에러내용 : ' + rsp.error_msg;
-            }
-            alert(msg);
-        });
+        try {
+            var pay_method_value = document.querySelector('input[name="pay_method"]:checked').value
+            var IMP = window.IMP; // 생략가능
+            IMP.init('imp40035237'); // 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
+            IMP.request_pay({
+                pg: 'inicis', // version 1.1.0부터 지원.
+                pay_method: pay_method_value,
+                merchant_uid: 'merchant_' + new Date().getTime(),
+                name: '주문명:진료비결제',
+                amount: @json($storage) ,
+                buyer_name: @json($patient->patient_name),
+                buyer_tel: @json($patient->phone_number),
+                buyer_addr: @json($patient->address),
+                buyer_postcode: @json($patient->postal_code),
+                m_redirect_url: 'http://34.234.79.156/index.php/patient/iamport_end/' + @json($patient->patient_id)
+            }, function(rsp) {
+                if (rsp.success) {
+                    var msg = '결제가 완료되었습니다.';
+                    msg += '고유ID : ' + rsp.imp_uid;
+                    msg += '상점 거래ID : ' + rsp.merchant_uid;
+                    msg += '결제 금액 : ' + rsp.paid_amount;
+                    msg += '카드 승인번호 : ' + rsp.apply_num;
+                } else {
+                    var msg = '결제에 실패하였습니다.';
+                    msg += '에러내용 : ' + rsp.error_msg;
+                }
+                alert(msg);
+            });
+        } catch (error) {
+            alert("결제수단을 선택해주세요");
+        }    
     }
 </script>
 </body>
