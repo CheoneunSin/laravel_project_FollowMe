84b98f80e28368d3e2895cdb5387b3beb00f0791
sincheneun <qkskskzlr112@naver.com>
2021-05-18 08:29:37 +0900

    aws git pull test2

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-//aws pull test
+//aws pull test2
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
