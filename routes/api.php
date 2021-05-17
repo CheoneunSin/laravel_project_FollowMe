c41e8b7c4644d8c215cac90cc6c1bfb2e6e11773
sincheneun <qkskskzlr112@naver.com>
2021-05-18 08:32:36 +0900

    aws pull test 마지막

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-//aws pull test2
+//aws pull test 마지막
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
